9353c56c73901e2fe8809af7af6f38052704f940
Th3Hacker <hamza.annane.official@gmail.com>
2025-02-15 15:04:21 +0100

    week01 pset0 fix bugs

--- week01/deep/deep.py
+++ week01/deep/deep.py
@@ -1,14 +1,13 @@
 
 answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
-if answer == "42":
+if answer.strip() == "42":
     print("Yes")
 
-elif answer == "forty-two":
+elif answer.lower().strip() == "forty-two":
      print("Yes")
 
-elif answer == "forty two":
+elif answer.lower().strip() == "forty two":
     print("Yes")
 
 else:
     print("No")
-
